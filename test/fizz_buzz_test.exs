8210b969899baf69e053b6e891197eb1d6683d7b
Peterson Fonseca <petersonsimiao1@gmail.com>
2020-08-26 02:55:53 -0300

    feat :sparkles: Criado testes para a função de build

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,39 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response =
+        {:ok,
+         [
+           1,
+           2,
+           :fizz,
+           4,
+           :buzz,
+           :fizz,
+           7,
+           8,
+           :fizz,
+           :buzz,
+           11,
+           :fizz,
+           13,
+           14,
+           :fizzbuzz,
+           16,
+           17,
+           :fizz,
+           19,
+           :buzz
+         ]}
+
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when is invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("invald.txt") == expected_response
+    end
   end
 end
